8632523252e33b1946b75c18d020db8b132b6add
kbility <kbility@10.0.0.122>
2018-10-22 22:42:54 -0400

    Implemented Map based Services, closes #6

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,39 @@
+package com.mohbility.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import com.mohbility.sfgpetclinic.model.Pet;
+import com.mohbility.sfgpetclinic.services.CrudService;
+
+/**
+ * @author kbility
+ * Date Oct 21, 2018
+ */
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+	 @Override
+	    public Set<Pet> findAll() {
+	        return super.findAll();
+	    }
+
+	    @Override
+	    public Pet findById(Long id) {
+	        return super.findById(id);
+	    }
+
+	    @Override
+	    public Pet save(Pet object) {
+	    	return super.save(object.getId(), object);	        
+	    }
+
+	    @Override
+	    public void delete(Pet object) {
+	        super.delete(object);
+	    }
+
+	    @Override
+	    public void deleteById(Long id) {
+	        super.deleteById(id);
+	    }
+
+}
---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,39 @@
+package com.mohbility.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import com.mohbility.sfgpetclinic.model.Vet;
+import com.mohbility.sfgpetclinic.services.CrudService;
+
+/**
+ * @author kbility
+ * Date Oct 21, 2018
+ */
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+	 @Override
+	    public Set<Vet> findAll() {
+	        return super.findAll();
+	    }
+
+	    @Override
+	    public Vet findById(Long id) {
+	        return super.findById(id);
+	    }
+
+	    @Override
+	    public Vet save(Vet object) {
+	    	return super.save(object.getId(), object);	        
+	    }
+
+	    @Override
+	    public void delete(Vet object) {
+	        super.delete(object);
+	    }
+
+	    @Override
+	    public void deleteById(Long id) {
+	        super.deleteById(id);
+	    }
+
+}
