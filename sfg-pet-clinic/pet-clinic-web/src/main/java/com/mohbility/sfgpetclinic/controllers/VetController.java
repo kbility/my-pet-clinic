7f9a4b7c30bb1a6b3356e9a44fba97a52b9a6c82
kbility <kbility@10.0.0.122>
2018-10-25 22:16:14 -0400

    Listed all Vets on Vet Index Page. Closes #15

--- sfg-pet-clinic/pet-clinic-web/src/main/java/com/mohbility/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/com/mohbility/sfgpetclinic/controllers/VetController.java
@@ -1,7 +1,10 @@
 package com.mohbility.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mohbility.sfgpetclinic.services.VetService;
 
 /**
  * @author kbility
@@ -11,8 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
 	
+	private final VetService vetService;
+	
+	/**
+	 * @param vetService
+	 */
+	public VetController(VetService vetService) {
+	 this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-	public String listVets() {
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
