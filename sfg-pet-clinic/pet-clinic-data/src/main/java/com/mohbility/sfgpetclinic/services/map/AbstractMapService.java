d5b02602e64d6c0b01815485f23fae81d16aca7b
kbility <kbility@10.0.0.122>
2018-10-25 23:59:03 -0400

    Refactored Map Impl to abstract ID generation. Closes #23

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,18 +1,23 @@
 package com.mohbility.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
+import javax.management.RuntimeErrorException;
+
+import com.mohbility.sfgpetclinic.model.BaseEntity;
 
 
 /**
  * @author kbility
  * Date Oct 21, 2018
  */
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 	
-	protected Map<ID, T> map = new HashMap<>();
+	protected Map<Long, T> map = new HashMap<>();
 	
 	Set<T> findAll(){
 		return new HashSet<>(map.values());
@@ -22,8 +27,15 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 	
-	T save(ID id,T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(object != null) {
+			if(object.getId()==null) {
+				object.setId(getNextId());
+			}
+			map.put(object.getId(), object);
+		} else {
+			throw new RuntimeErrorException(null, "Object cannot be null");
+		}
 		return object;
 	}
 	
@@ -33,6 +45,16 @@ public abstract class AbstractMapService<T, ID> {
 	
 	void delete(T object) {
 		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+	}
+	
+	private Long getNextId() {
+		Long nextId = null;
+		try {
+			nextId = Collections.max(map.keySet())+ 1;
+		} catch (Exception e) {
+			nextId = 1L;
+		}
+		return nextId; 
 	}
 
 
