50464f5fc07b33d17b3110d66dcaacf21fa45079
kbility <kbility@10.0.0.122>
2018-11-02 01:53:13 -0400

    Added visits, updated owner with pet relationship. Closes #26

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/mohbility/sfgpetclinic/model/Owner.java
@@ -3,16 +3,27 @@
  */
 package com.mohbility.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author kbility
  * Date Oct 14, 2018
  */
+@SuppressWarnings("serial")
 public class Owner extends Person {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
+	}
+	
+	
 		
 
 }
